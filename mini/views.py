9c66bb90d210808df00096505ee83c5bd84c6ef8
George <glychock@gmail.com>
2021-12-05 23:21:24 -0500

    version: Completing the Mini project for class

--- mini/views.py
+++ mini/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import redirect, render, redirect
+from django.shortcuts import redirect, render, redirect, get_object_or_404
 from .models import Item
-
-# Create your views here.
+from .forms import ItemForm
 
 
 def home_page(request):
@@ -14,9 +13,39 @@ def home_page(request):
 
 def add_item(request):
     if request.method == 'POST':
-        name = request.POST.get('item_name')
-        done = 'done' in request.POST
-        Item.objects.create(name=name, doen=done)
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'mini/add_item.html', context)
 
-        return redirect('home')
-    return render(request, 'mini/add_item.html')
+
+def edit_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    form = ItemForm(instance=item)
+    context = {
+        'form': form
+    }
+    return render(request, 'mini/edit_item.html', context)
+
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.doen = not item.doen
+    item.save()
+    return redirect('home')
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('home')
