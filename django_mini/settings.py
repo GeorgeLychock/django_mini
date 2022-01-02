03ce56b0221afeb5c414e9741b4f117d1ce44c6d
George <glychock@gmail.com>
2022-01-02 01:33:14 -0500

    initiate: Prepare everything to deploy to Heroku

--- django_mini/settings.py
+++ django_mini/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_mini.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://iuvmtkjxfamqzv:9be9088eaa75fae88e47d15b845942cc1bee7ace8e0dfda0329ad8555d83aabe@ec2-3-227-154-49.compute-1.amazonaws.com:5432/dd1rfj73c7p4e5')
 }
 
 
