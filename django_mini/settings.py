9a504b260871bfd24b5508555026bb17011ddfc6
George <glychock@gmail.com>
2022-01-04 09:49:54 -0500

    Issue: Hiding secret key

--- django_mini/settings.py
+++ django_mini/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+import env as env
 
 development = os.environ.get('DEVELOPMENT', False)
 
@@ -23,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '5w*q-r3g-$u-abb5jxgts$t!k*y0!sq_g1_n=s5bbwx!1xoow4')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
 if development:
-    ALLOWED_HOSTS = ['127.0.0.0']
+    ALLOWED_HOSTS = ['127.0.0.1']
 else:
     ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
