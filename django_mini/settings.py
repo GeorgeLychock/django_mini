5363ffa63d8d1f254cb5f16651a988a460cc9334
George <glychock@gmail.com>
2022-01-08 08:29:08 -0500

    testing: Making dev env changes

--- django_mini/settings.py
+++ django_mini/settings.py
@@ -12,7 +12,7 @@ import dj_database_url
 from pathlib import Path
 import env as env
 
-development = os.environ.get('DEVELOPMENT', False)
+development = os.environ.get('DEVELOPMENT', "False")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
