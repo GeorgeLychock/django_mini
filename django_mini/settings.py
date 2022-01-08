4d310115a25f270cd2b513ce40bbcd0f15d22252
George <glychock@gmail.com>
2022-01-08 08:12:04 -0500

    testing: recreated s/k env var in Heroku

--- django_mini/settings.py
+++ django_mini/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5d=wfn!vb_kfgp2u$y*l^v-jab@#x_0(n$)5szm1hj5)zz0$y8'
+# SECRET_KEY = '5d=wfn!vb_kfgp2u$y*l^v-jab@#x_0(n$)5szm1hj5)zz0$y8'
+SECRET_KEY = os.environ.get('SECRET_KEY', '5d=wfn!vb_kfgp2u$y*l^v-jab@#x_0(n$)5szm1hj5)zz0$y8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
